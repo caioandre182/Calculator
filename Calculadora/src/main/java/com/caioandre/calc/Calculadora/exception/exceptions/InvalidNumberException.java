80622aaea24c4615895f463a821bf90eb20d219b
caioandre182 <caioserralvo182@gmail.com>
2023-03-16 21:40:22 -0300

    feat: criação das classes Exceptions para validação de entrada da api.

---
+++ Calculadora/src/main/java/com/caioandre/calc/Calculadora/exception/exceptions/InvalidNumberException.java
@@ -0,0 +1,9 @@
+package com.caioandre.calc.Calculadora.exception.exceptions;
+
+public class InvalidNumberException extends RuntimeException{
+
+    public InvalidNumberException(String message){
+        super(message);
+    }
+
+}
---
+++ Calculadora/src/main/java/com/caioandre/calc/Calculadora/exception/exceptions/NumberNotFoundException.java
@@ -0,0 +1,9 @@
+package com.caioandre.calc.Calculadora.exception.exceptions;
+
+public class NumberNotFoundException extends RuntimeException {
+
+    public NumberNotFoundException(String message){
+        super(message);
+    }
+
+}
