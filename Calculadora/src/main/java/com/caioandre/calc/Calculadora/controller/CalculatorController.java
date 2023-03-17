40e8fa6ac92891f167c028d4ae9339f8619103d4
caioandre182 <caioserralvo182@gmail.com>
2023-03-16 21:33:21 -0300

    feat: criação do controller da Api, com os endpoints add(somar) e sub(subtrair)

---
+++ Calculadora/src/main/java/com/caioandre/calc/Calculadora/controller/CalculatorController.java
@@ -0,0 +1,24 @@
+package com.caioandre.calc.Calculadora.controller;
+
+import com.caioandre.calc.Calculadora.entity.Calculator;
+import com.caioandre.calc.Calculadora.service.ICalculatorService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/calc")
+@AllArgsConstructor
+public class CalculatorController {
+
+    private final ICalculatorService service;
+
+    @PostMapping(value = "/add")
+    public String add(@RequestBody Calculator calculator) {
+        return service.add(calculator);
+    }
+
+    @PostMapping(value = "/sub")
+    public String subtract(@RequestBody Calculator calculator) {
+        return service.subtract(calculator);
+    }
+}
