5fb80955652d8b2fb6167760f067656b82585b5b
caioandre182 <caioserralvo182@gmail.com>
2023-03-16 21:04:45 -0300

    feat: criação da classe Calculator.java no pacote entity

---
+++ Calculadora/src/main/java/com/caioandre/calc/Calculadora/entity/Calculator.java
@@ -0,0 +1,14 @@
+package com.caioandre.calc.Calculadora.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Calculator {
+
+    private Double number1;
+    private Double number2;
+}
