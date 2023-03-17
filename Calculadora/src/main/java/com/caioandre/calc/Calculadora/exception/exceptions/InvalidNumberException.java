0e3ba0b3c6fee4d1e54b7abe1f9e50ce958a268a
caioandre182 <caioserralvo182@gmail.com>
2023-03-16 22:32:07 -0300

    refactor: exclusão da exception InvalidNumberException.java, já existe uma exception com o mesmo propósito

--- Calculadora/src/main/java/com/caioandre/calc/Calculadora/exception/exceptions/InvalidNumberException.java
+++
@@ -1,9 +0,0 @@
-package com.caioandre.calc.Calculadora.exception.exceptions;
-
-public class InvalidNumberException extends RuntimeException{
-
-    public InvalidNumberException(String message){
-        super(message);
-    }
-
-}
